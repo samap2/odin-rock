860e7ca55f7eb09b5d967fcafa0d8a74bf94e437
samap2 <samuelmosesbala@gmail.com>
2023-06-16 10:16:26 +0100

    get-computer-choice

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 console.log('Hi')
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)]
+    console. log();
+    return choice;
+}
+
+function checkwinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
+        return "Tie";    
+    } 
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else{
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkwinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "it's a Tie!"
+    }
+    else if(result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console. log(playRound(playerSelection,computerSelection));
